b811124ebd5bd08d00609be7aa466b6ee7bf526c
Taponidhi Tulsi <taponidhi@curefit.com>
2021-09-21 16:12:26 +0530

    Fix

--- sync_managed_microcycle/sync_managed_microcycle.py
+++ sync_managed_microcycle/sync_managed_microcycle.py
@@ -18,8 +18,9 @@ def get_plan_maps(plan_maps_filename):
 
 async def sync_plan(session, request_body):
     url = "http://user-fitness-service.production.cure.fit.internal/sync/managed_microcycle_single_user"
+    headers = {'content-type': 'application/json'}
 
-    async with session.put(url, data=request_body) as res:
+    async with session.put(url, data=request_body, headers = headers) as res:
         status = await res.json()
         print(status)
 
